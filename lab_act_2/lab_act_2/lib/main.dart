c8e00963fbcb4c382444c3fd9d8cc726f4c6a208
LAB12_USER ON WS25 <L12X16W25@LabHCI.net>
2026-08-26 16:06:09 +0800

    add image

--- lab_act_2/lab_act_2/lib/main.dart
+++ lab_act_2/lab_act_2/lib/main.dart
@@ -7,12 +7,25 @@ void main() {
         body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
-              Colors.blueAccent,
-              Colors.amberAccent
-            ])
+              const Color.fromARGB(255, 68, 255, 109),
+              const Color.fromARGB(255, 64, 255, 107)
+            ]) 
           ),
-          child: Center(
-            child: Text("Hello world"),
+          child: Center(  
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                 Image.asset(
+                  width: 200,
+                  'assets/dice-images/dice-3.png'),
+                  SizedBox(height: 25),
+                 TextButton(onPressed: () {}, child: Text(
+                  style: TextStyle(
+                    fontSize: 28
+                  ),
+                  "Roll Dice"))
+              ],
+            ) 
           ),
         ),
       ),
