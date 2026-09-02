ffef47f8b664cd4f11f793925200de4464b89c1b
Rey Galo <reygalo29@gmail.com>
2026-09-02 17:13:05 +0800

    Create and Added quiz_screen.dart

--- lab_act_2/quiz_app/lib/main.dart
+++ lab_act_2/quiz_app/lib/main.dart
@@ -1,46 +1,50 @@
-
 import 'package:flutter/material.dart';
+
+import 'quiz_screen.dart';
 
 var bgImage = 'assets/logo.png';
 
-void main () {
+void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 72, 0, 144)
-          ),
+          decoration: BoxDecoration(color: Color.fromARGB(255, 72, 0, 144)),
           child: Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Image.asset(bgImage, width: 200,),
+                Image.asset(bgImage, width: 200),
                 SizedBox(height: 25),
-                Text('Learn flutter the fun way!',
+                Text(
+                  'Learn flutter the fun way!',
                   style: TextStyle(
                     color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 17,
-                  fontWeight: FontWeight.w500
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
                 SizedBox(height: 30),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => QuizScreen()),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Color.fromARGB(255, 44, 0, 88),
                   ),
-                   child: Text('Start Quiz',
-                   style: TextStyle(
-                    color: Colors.white
-                   ),
-                   )
-                   ),
+                  child: Text(
+                    'Start Quiz',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ],
-            )
+            ),
           ),
         ),
       ),
-    )
+    ),
   );
 }
