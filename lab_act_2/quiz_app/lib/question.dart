15c091f10328acba69e6b8a637dac3baad153d1e
Rey Galo <reygalo29@gmail.com>
2026-09-04 20:20:50 +0800

    Added question.dart class for the questions blueprint

---
+++ lab_act_2/quiz_app/lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  final String questionText;
+  final List<String> choices;
+  final int correctAnswerIndex;
+
+  Question({
+    required this.questionText,
+    required this.choices,
+    required this.correctAnswerIndex,
+  });
+}
